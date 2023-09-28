1be4dd7cdd689cc219c35c9faea843fc46930025
Swift <swiftx37@gmail.com>
2023-09-28 11:59:21 +0200

    Changing country flags when choosing a currency

--- main.js
+++ main.js
@@ -6,6 +6,17 @@ const outputCurrency = document.getElementById('outputCurrency')
 const inputAmount = document.getElementById('inputAmount')
 const outputAmount = document.getElementById('outputAmount')
 
+const loadFlag = target => {
+	for (currency_code in country_codes) {
+		if (currency_code === target.value) {
+			const img = target.parentElement.querySelector('img')
+			img.src = `https://hatscripts.github.io/circle-flags/flags/${country_codes[
+				currency_code
+			].toLowerCase()}.svg`
+		}
+	}
+}
+
 dropdowns.forEach(element => {
 	for (currency_code in country_codes) {
 		let selected
@@ -17,6 +28,8 @@ dropdowns.forEach(element => {
 		let option = `<option value="${currency_code}" ${selected}>${currency_code}</option>`
 		element.insertAdjacentHTML('beforeend', option)
 	}
+
+	element.addEventListener('change', e => loadFlag(e.target))
 })
 
 const getExchangeRate = () => {
